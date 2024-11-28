9ad9bef184df2c28e0834c5f3d5a979bac3b9d09
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-11-28 17:28:02 +0530

    Update input_schema.py

--- input_schema.py
+++ input_schema.py
@@ -18,37 +18,37 @@ INPUT_SCHEMA = {
         'example': ["You are a helpful coding bot."]
     },
     "temperature": {
-        'datatype': 'FP32',
+        'datatype': 'FP64',
         'required': False,
         'shape': [1],
         'example': [0.7]
     },
     "top_p": {
-        'datatype': 'FP32',
+        'datatype': 'FP64',
         'required': False,
         'shape': [1],
         'example': [0.1]
     },
     "repetition_penalty": {
-        'datatype': 'FP32',
+        'datatype': 'FP64',
         'required': False,
         'shape': [1],
         'example': [1.18]
     },
     "max_tokens": {
-        'datatype': 'INT16',
+        'datatype': 'INT64',
         'required': False,
         'shape': [1],
         'example': [256]
     },
     "max_pixels": {
-        'datatype': 'INT16',
+        'datatype': 'INT64',
         'required': False,
         'shape': [1],
         'example': [12845056]
     },
     "top_k":{
-        'datatype': 'INT8',
+        'datatype': 'INT64',
         'required': False,
         'shape': [1],
         'example': [40]
