0880a391d54fec3e22e5710f1b44ecc6385098d5
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-11-28 18:23:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ class InferlessPythonModel:
         content_url = inputs["content_url"]
         content_type = inputs.get("content_type","image")
         system_prompt = inputs.get("system_prompt","You are a helpful assistant.")
-        temperature = inputs.get("temperature",0.7)
-        top_p = inputs.get("top_p",0.1)
-        repetition_penalty = inputs.get("repetition_penalty",1.18)
-        top_k = inputs.get("top_k",40)
-        max_tokens = inputs.get("max_tokens",256)
-        max_pixels = inputs.get("max_pixels",12845056)
-        max_duration = inputs.get("max_duration",60)
+        temperature = float(inputs.get("temperature",0.7))
+        top_p = float(inputs.get("top_p",0.1))
+        repetition_penalty = float(inputs.get("repetition_penalty",1.18))
+        top_k = int(inputs.get("top_k",40))
+        max_tokens = int(inputs.get("max_tokens",256))
+        max_pixels = int(inputs.get("max_pixels",12845056))
+        max_duration = int(inputs.get("max_duration",60))
 
         sampling_params = SamplingParams(temperature=temperature,top_p=top_p,repetition_penalty=repetition_penalty,
                                          top_k=top_k,max_tokens=max_tokens)
